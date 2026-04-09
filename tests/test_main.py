d07b4300b538f14337480f52b2e281ac0f14cdeb
Omar Ali Khan <oakhan3@gmail.com>
2026-04-08 20:06:32 -0400

    chore: Update gitignore and move test into class.

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,9 +1,6 @@
-import pytest
-
 from friend.main import main
 
 
-def test_main(capsys: pytest.CaptureFixture[str]) -> None:
-    main()
-    captured = capsys.readouterr()
-    assert captured.out == "Hello from friend!\n"
+class TestMain:
+    def test_main(self) -> None:
+        main()
